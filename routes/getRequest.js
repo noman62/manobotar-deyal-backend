2254320cf670081f9a279126e939cb6a659da03f
noman62 <noman.ice.nstu@gmail.com>
2022-10-03 05:59:03 -0700

    add midleware

--- routes/getRequest.js
+++ routes/getRequest.js
@@ -1,8 +1,8 @@
 import express from 'express'
 const router = express.Router()
 
-// middleware
-// import { requireSignin } from '../middlewares'
+middleware
+import { requireSignin } from '../middlewares'
 
 // controllers
 import {
@@ -14,7 +14,7 @@ adminRequest
 // Routes
 
 
-router.post('/request',newRequest)
+router.post('/request',requireSignin,newRequest)
 router.get('/requests', allRequest)
 router.get('/allrequest', adminRequest)
 // router.get('/donation/:donationId', read)
